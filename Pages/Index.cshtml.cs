a966451cc48dc5f92230af829636364222207bc2
Manoj Ravikumar Nair <Manoj.Nair@outlook.com>
2023-04-04 22:50:22 +1000

    Initial Commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    private readonly IHttpContextAccessor _contextAccessor;
+    public string IPAddress { get; set; }
+
+    public IndexModel(ILogger<IndexModel> logger, IHttpContextAccessor contextAccessor)
+    {
+        _logger = logger;
+        _contextAccessor = contextAccessor;
+    }
+
+    public void OnGet()
+    {
+       IPAddress=  _contextAccessor.HttpContext.Connection.RemoteIpAddress.ToString();
+        
+    }
+}
